1f6541ea03a36de11e971e01d4f78b09bda525e8
AmetJunior <ametsejdaliev@gmail.com>
2023-07-22 21:46:24 +0300

    main2

--- main2.py
+++ main2.py
@@ -6,7 +6,13 @@ i = 277
 n = 1000
 pg.doubleClick(1559, 28)
 time.sleep(10)
+pg.leftClick(58, 277)
+
 while i <= n:
     i = i + 20
-    pg.leftClick(58, i)
-
+    print(i)
+    pg.hotkey("ctrl", "c")
+    keyboard.press("down")
+    time.sleep(5)
+    pg.leftClick(806, 882)
+    time.sleep(5)
